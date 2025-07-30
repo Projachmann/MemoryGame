1720bdaae66a05a7ee38371e5718fc7f9916aa65
Jachym Fukal <pro.jachmann@gmail.com>
2025-07-30 07:48:45 +0200

    First commit
    First time using Gnity and Github together.

---
+++ Memory Game/Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+public class MemoryCard : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("turn around");
+    }
+}
